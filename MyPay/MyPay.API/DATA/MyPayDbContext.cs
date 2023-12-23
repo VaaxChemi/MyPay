18d53a0c50bad428813331c47ef6cd170437322a
visual studio <vs@itstep.ge>
2023-12-23 19:29:44 +0400

    updated

---
+++ MyPay/MyPay.API/DATA/MyPayDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MyPay.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyPay.API.DATA
+{
+    public class MyPayDbContext : DbContext
+    {
+        public MyPayDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<User> User { get; set; }
+        public DbSet<Card> Card { get; set; }
+        public DbSet<User> Activity { get; set; }
+        
+    }
+}
+
+
